cb60fc9fb5045793369daa1685b1b38a1236a606
Chanhee <cris.j.dev@gmail.com>
2022-09-26 14:46:48 +0900

    update cluster

---
+++ MapContainer.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+
+const MapContainer = () => {
+    return (
+        <div id='myMap' style={{
+            width: '500px', 
+            height: '500px'
+        }}></div>
+    );
+}
+
+export default MapContainer; 
